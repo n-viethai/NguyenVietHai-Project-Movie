f01b3b792365b4ebc5a7a6ef3b8fa4a6ff4378f0
HaiNguyen <viethai1309@gmail.com>
2021-09-08 00:45:31 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserHistory } from "history";
+
+export const history = createBrowserHistory();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="text-4xl text-red-500">Test</p>
     </div>
   );
 }
