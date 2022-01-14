d3ba8431e15d8ffdc4d56bcd61b81f70af3c260c
HaiNguyen <viethai1309@gmail.com>
2022-01-14 09:46:19 +0700

    new file

---
+++ src/Components/index.js
@@ -0,0 +1,4 @@
+export { default as FilmComponent } from "./Film/Film.jsx";
+export { default as LoadingComponent } from "./Loading/Loading";
+export { default as ScrollToTopComponent } from "./ScrollToTop/ScrollToTop";
+export { default as MultipleRowComponent } from "./ReactSlick/MultipleRow";
